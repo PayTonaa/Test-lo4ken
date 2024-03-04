c33e4c3c99d38a8e586bcd823be493c23921ea85
Bartek <139969478+PayTonaa@users.noreply.github.com>
2024-03-04 11:53:03 +0100

    sortowanie.py

--- course - basics = functions/sortowanie.py
+++ course - basics = functions/sortowanie.py
@@ -2,8 +2,10 @@
 import random
 # def sortuj(tab):
 #     n = len(tab)
+#     ile = 0
 #     for i in range(n):
 #         for j in range(0, n-i-1):
+#          ile += 1
 #             if tab[j] > tab[j+1]:
 #                 tab[j], tab[j+1] = tab[j+1], tab[j]
 #
@@ -17,16 +19,21 @@ import random
 
 def sortuj2(tab):
     n = len(tab)
+    ile = 0
     for i in range(n):
         lmin = i
         for j in range(i + 1, n):
+            ile += 1
             if tab[j] < tab[lmin]:
                 lmin = j
+
         tab[i], tab[lmin] = tab[lmin], tab[i]
+    return ile
 
 tab = []
 for x in range(10):
     tab.append(random.randint(0, 100000))
 print(tab)
 sortuj2(tab)
+print(sortuj2(tab))
 print(tab)
